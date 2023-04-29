d01e532deaa56b929b63eb60736ad96218d7a964
Mathido <mdovalecds@gmail.com>
2023-04-29 00:59:11 -0300

    Solving bug in config.ini deleting str and removing handler in loggen

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -1,10 +1,11 @@
 import logging
 
-class LogGen():
-
+class LogGen:
     @staticmethod
     def loggen():
-        logging.basicConfig(filename=".\\logs\\automation.log", 
+        for handler in logging.root.handlers[:]:
+            logging.root.removeHandler(handler)
+        logging.basicConfig(filename=".\\logs\\automation.log",
                             format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
         logger=logging.getLogger()
         logger.setLevel(logging.INFO)
